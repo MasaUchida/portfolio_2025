6311337bfd6e344b4d6d7c78107c4bb586707a32
Uchida Masatoshi <you@example.com>
2024-12-06 19:13:45 +0900

    aboutページ作成。アクセスできることを確認

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AboutPage: React.FC = () => {
+  return (
+    <>
+      <main>
+        <p>自己紹介のページだよ</p>
+      </main>
+    </>
+  );
+};
+
+export default AboutPage;
