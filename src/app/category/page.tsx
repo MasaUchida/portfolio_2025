f6cd6c0498465b6001414c1659eefb43ebe0808f
Uchida Masatoshi <you@example.com>
2024-12-06 14:04:17 +0900

    categoryのフォルダを作成しpageを作った

---
+++ src/app/category/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <main>
+        <p>カテゴリのページだよ</p>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
