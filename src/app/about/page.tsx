322fff0b3f69f5c5ee3910ec9e6cd9d33972b366
Uchida Masatoshi <you@example.com>
2024-12-10 17:35:54 +0900

    aboutページにヘッダーとフッターを忘れていたので追加

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
 const AboutPage: React.FC = () => {
   return (
     <>
+      <Header />
       <main>
         <p>自己紹介のページだよ</p>
       </main>
+      <Footer />
     </>
   );
 };
