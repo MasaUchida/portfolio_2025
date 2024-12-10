ec6f212c3ed836c34922ad49a270ce5d97c57768
Uchida Masatoshi <you@example.com>
2024-12-10 15:37:05 +0900

    ヘッダー・フッター以外の基礎完成

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,15 +1,33 @@
 import React from "react";
 import Card from "../../components/Card";
+import Image from "next/image";
+import Tag from "../../components/Tag";
 
 const WorksPage: React.FC = () => {
   return (
     <>
       <main>
-        <p>作品ページだよ</p>
-        <div className="flex gap-12">
+        <div>
+          <Image
+            src={"/works-title.png"}
+            alt="#"
+            width={448}
+            height={112}
+          ></Image>
+        </div>
+        <div className="flex gap-6">
+          <span>フィルタ</span>
+          <Tag id="1" tagName="Product"></Tag>
+          <Tag id="2" tagName="Project"></Tag>
+          <Tag id="3" tagName="WorkShop"></Tag>
+        </div>
+        <div className="flex gap-12 flex-wrap">
           <Card id="1" title="カード1"></Card>
-          <Card id="1" title="カード1"></Card>
-          <Card id="1" title="カード1"></Card>
+          <Card id="2" title="カード2"></Card>
+          <Card id="3" title="カード3"></Card>
+          <Card id="4" title="カード4"></Card>
+          <Card id="5" title="カード5"></Card>
+          <Card id="6" title="カード6"></Card>
         </div>
       </main>
     </>
