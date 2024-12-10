7518b4a244cf7688369919697c2981a3881406ac
Uchida Masatoshi <you@example.com>
2024-12-10 17:32:17 +0900

    フッターのスタイルを修正

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,17 +3,37 @@ import Link from "next/link";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="border-t-2 border-black bg-gray-100">
+    <footer className="px-20 py-10 flex gap-20 border-t-2 border-black bg-gray-100">
       <div>
-        <h3>サイトマップ</h3>
-        <Link href={"/"}>Home</Link>
-        <Link href={"/works"}>Works</Link>
-        <Link href={"/about"}>About</Link>
+        <h3 className="font-bold text-lg">サイトマップ</h3>
+        <div className="pt-6 flex flex-col gap-3">
+          <Link href={"/"} className="text-sm underline">
+            Home
+          </Link>
+          <Link href={"/works"} className="text-sm underline">
+            Works
+          </Link>
+          <Link href={"/about"} className="text-sm underline">
+            About
+          </Link>
+        </div>
       </div>
       <div>
-        <h3>関連メディア</h3>
-        <Link href={"https://uchida-design.vercel.app/"}>旧ポートフォリオ</Link>
-        <Link href={"https://x.com/uchi__design"}>X</Link>
+        <h3 className="font-bold text-lg">関連メディア</h3>
+        <div className="pt-6 flex flex-col gap-3">
+          <Link
+            href={"https://uchida-design.vercel.app/"}
+            className="text-sm underline"
+          >
+            旧ポートフォリオ
+          </Link>
+          <Link
+            href={"https://x.com/uchi__design"}
+            className="text-sm underline"
+          >
+            X
+          </Link>
+        </div>
       </div>
     </footer>
   );
