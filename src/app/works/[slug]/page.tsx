43e2a00d4c37162b786e7c55e5621e00d1b90db0
Uchida Masatoshi <you@example.com>
2024-12-10 18:13:33 +0900

    Postページができた

--- src/app/works/[slug]/page.tsx
+++ src/app/works/[slug]/page.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { getAllPosts } from "../../../../libs/dataFetch";
+import Image from "next/image";
+import Card from "../../../components/Card";
 import Header from "../../../components/Header";
 import Footer from "../../../components/Footer";
+import Tag from "../../../components/Tag";
 
 type returnParamsValueType = { slug: string };
 type paramsType<T> = { params: Promise<T> };
@@ -13,9 +16,39 @@ const PostPage = async ({ params }: paramsType<returnParamsValueType>) => {
   return (
     <>
       <main>
-        <Header />
-        <p>postページだよ</p>
-        <p>{`${uriString}`}</p>
+        <div className="p-10 flex gap-10">
+          <div className="w-full flex flex-col gap-6">
+            <div className="h-10 border-2 border-black rounded-full">
+              Home&gt;works&gt;タイトル
+            </div>
+            <div className="flex items-center">
+              <h1 className="text-4xl font-bold flex-grow">タイトル</h1>
+              <p>プロジェクト期間：</p>
+              <Tag id="1" tagName="Product" size="large" />
+            </div>
+            <Image
+              src={"/#"}
+              alt="#"
+              width={920}
+              height={460}
+              className="bg-gray-300 rounded-3xl"
+            />
+            <div>
+              <p>postの本体だよ</p>
+              <p>{`${uriString}`}</p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center gap-10">
+            <Header />
+            <div className="flex flex-col gap-4">
+              <Card id="1" title="サイドカード1" />
+              <Card id="1" title="サイドカード2" />
+              <Card id="1" title="サイドカード3" />
+              <Card id="1" title="サイドカード4" />
+            </div>
+          </div>
+        </div>
+
         <Footer />
       </main>
     </>
