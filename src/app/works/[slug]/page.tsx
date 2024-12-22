34bef05e49472ab6c44ebc261def875a242152be
Masatoshi Uchida <uchidamasa.design@gmail.com>
2024-12-23 02:18:54 +0900

    スタイルとかちまちま直下

--- src/app/works/[slug]/page.tsx
+++ src/app/works/[slug]/page.tsx
@@ -26,67 +26,73 @@ const PostPage = async ({ params }: paramsType<returnParamsValueType>) => {
   return (
     <>
       <main>
-        <div className="p-10 flex gap-10">
-          <div className="w-full flex flex-col gap-6">
-            <div className="px-4 h-10 border-2 border-gray-900 rounded-full leading-10">
-              <Link href={"/"}>Home</Link>
-              <span> &gt; </span>
-              <Link href={"/works"}>Works</Link>
-              <span> &gt; </span>
-              <span>{`${postContents.title}`}</span>
-            </div>
-            <div className="flex items-center gap-4">
-              <h1 className="text-4xl font-bold flex-grow">{`${postContents.title}`}</h1>
-              <p>プロジェクト期間：{`${postContents.projectPeriod}`}</p>
-              {targetTagObjects.map((tag: TargetTagType) => {
-                return (
-                  <Tag
-                    key={tag.tagId}
-                    id={tag.tagId}
-                    tagName={`${tag.tagName}`}
-                    size="large"
-                  />
-                );
-              })}
-            </div>
-            <Image
-              src={`${
-                postContents.postImage
-                  ? postContents.postImage.url
-                  : "/test.png"
-              }`}
-              alt="#"
-              width={960}
-              height={480}
-              className="bg-gray-300 rounded-3xl border-2 border-gray-900"
-            />
-            <div
-              className="post-discription"
-              dangerouslySetInnerHTML={{ __html: postContents.description }}
-            ></div>
-          </div>
-          <div className="flex flex-col items-center gap-10">
-            <Header />
-            <div className="flex flex-col gap-4">
-              {sideCardPosts
-                .filter((post) => {
-                  return post.postUri !== uriString.slug;
-                })
-                .map((post) => {
-                  console.log(post.carouselImage);
+        <div className="px-10 pt-10 pb-20  flex justify-center">
+          <div className="flex gap-10 max-w-[1200px]">
+            <div className="w-full flex flex-col gap-6">
+              <div className="px-4 h-10 border-2 border-gray-900 rounded-full leading-10">
+                <Link href={"/"}>Home</Link>
+                <span> &gt; </span>
+                <Link href={"/works"}>Works</Link>
+                <span> &gt; </span>
+                <span>{`${postContents.title}`}</span>
+              </div>
+              <div className="flex items-center gap-4">
+                <h1 className="text-4xl font-bold flex-grow">{`${postContents.title}`}</h1>
+                <p>プロジェクト期間：{`${postContents.projectPeriod}`}</p>
+                {targetTagObjects.map((tag: TargetTagType) => {
                   return (
-                    <Link href={`/works/${post.postUri}`} key={`${post.id}`}>
-                      <Card
-                        id={`${post.id}`}
-                        title={`${post.title}`}
-                        imageUrl={`${
-                          post.carouselImage ? post.carouselImage.url : ""
-                        }`}
-                        description=" "
-                      />
-                    </Link>
+                    <Tag
+                      key={tag.tagId}
+                      id={tag.tagId}
+                      tagName={`${tag.tagName}`}
+                      size="large"
+                    />
                   );
                 })}
+              </div>
+              <Image
+                src={`${
+                  postContents.postImage
+                    ? postContents.postImage.url
+                    : "/test.png"
+                }`}
+                alt="#"
+                width={960}
+                height={480}
+                className="bg-gray-300 rounded-3xl border-2 border-gray-900"
+              />
+              <div
+                className="post-discription"
+                dangerouslySetInnerHTML={{ __html: postContents.description }}
+              ></div>
+            </div>
+            <div className="flex flex-col items-center gap-10">
+              <Header />
+              <div className="flex flex-col gap-4">
+                {sideCardPosts
+                  .filter((post) => {
+                    return post.postUri !== uriString.slug;
+                  })
+                  .map((post) => {
+                    console.log(post.carouselImage);
+                    return (
+                      <Link href={`/works/${post.postUri}`} key={`${post.id}`}>
+                        <Card
+                          id={`${post.id}`}
+                          title={`${post.title}`}
+                          imageUrl={`${
+                            post.carouselImage ? post.carouselImage.url : ""
+                          }`}
+                          projectPeriod={`${post.projectPeriod}`}
+                          tagNames={post.tags?.map(
+                            (tag: TagType) => tag.tagName
+                          )}
+                          description=" "
+                        />
+                      </Link>
+                    );
+                  })}
+              </div>
             </div>
           </div>
         </div>
